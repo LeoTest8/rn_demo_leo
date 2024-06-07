03c9d14d167608d590870bec6f2f2c8817419ff0
ZohorLeo <yakuman@45.78.51.232.16clouds.com>
2024-06-07 08:09:55 +0800

    test push

---
+++ PracticeComps/MyText.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+export default function MyText() {
+  return (
+    <View>
+      <Text>kjsdfiukj</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({});
